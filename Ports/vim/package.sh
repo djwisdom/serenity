1e2e3d35ddd482eccee0d784fc8340170a4e48eb
Dennis Esternon <djwisdom@gmail.com>
2024-12-07 07:11:01 +0800

    Ports: Add .vimrc to vim port

--- Ports/vim/package.sh
+++ Ports/vim/package.sh
@@ -22,4 +22,19 @@ export vim_cv_uname_m_output="${SERENITY_ARCH}"
 
 post_install() {
     run ln -sf vim "${SERENITY_INSTALL_ROOT}/usr/local/bin/vi"
+    
+    value=$(grep -c "syntax enable" ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc)
+
+    if [ $value -eq 0]; then
+        echo 'syntax enable' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'set tabstop=2 expandtab shiftwidth=2 smarttab' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'set number' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'set hlsearch' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'set incsearch' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'set cursorline' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'let g:netrw_liststyle=3' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'highlight LineNr term=bold ctermfg=darkgray ctermbg=NONE' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'highlight CursorLine cterm=bold ctermbg=NONE' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'highlightNR cterm=bold ctermfg=NONE ctermbg=NONE' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+    fi 
 }
