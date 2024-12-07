337af57028738c6ec4a21a5be3f2b5d2f27c1454
Dennis Esternon <djwisdom@gmail.com>
2024-12-07 10:10:59 +0800

    Ports: Update vim port update vimrc

--- Ports/vim/package.sh
+++ Ports/vim/package.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=vim
-version=8.2.4554
+version=8.2.5056
 workdir="${port}-${version}"
 useconfigure="true"
 files=(
-    "https://github.com/vim/vim/archive/refs/tags/v${version}.tar.gz#206c8fc2535df33b9ea62fa1c9acae66c981a3e3aa4de0f652ef3a25e2b92571"
+    "https://github.com/vim/vim/archive/refs/tags/v${version}.tar.gz#2a7ed006ba7c6af3210525606444a9c4bd45efb467e9df045eabc5a09bc428f9"
 )
-configopts=("--with-tlib=tinfo" "--with-features=normal")
+configopts=("--with-tlib=tinfo" "--with-features=huge")
 depends=("ncurses" "gettext")
 
 export vim_cv_getcwd_broken=no
@@ -23,9 +23,12 @@ export vim_cv_uname_m_output="${SERENITY_ARCH}"
 post_install() {
     run ln -sf vim "${SERENITY_INSTALL_ROOT}/usr/local/bin/vi"
     
-    value=$(grep -c "syntax enable" ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc)
+    # value=$(grep -c "syntax enable" ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc)
 
-    if [ $value -eq 0]; then
+    if [ -f "${SERENITY_INSTALL_ROOT}/home/anon/.vimrc" ]; then
+        echo 'Existing vimrc file found... skipping.'
+    else
+        echo 'Missing vimrc file. Autogenerating...'
         echo 'syntax enable' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
         echo 'set tabstop=2 expandtab shiftwidth=2 smarttab' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
         echo 'set number' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
@@ -33,8 +36,8 @@ post_install() {
         echo 'set incsearch' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
         echo 'set cursorline' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
         echo 'let g:netrw_liststyle=3' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
-        echo 'highlight LineNr term=bold ctermfg=darkgray ctermbg=NONE' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'highlight LineNr term=NONE ctermfg=darkgray ctermbg=NONE' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
         echo 'highlight CursorLine cterm=bold ctermbg=NONE' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
-        echo 'highlightNR cterm=bold ctermfg=NONE ctermbg=NONE' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
+        echo 'highlight CursorLineNr cterm=bold ctermfg=NONE ctermbg=NONE' >> ${SERENITY_INSTALL_ROOT}/home/anon/.vimrc
     fi 
 }
